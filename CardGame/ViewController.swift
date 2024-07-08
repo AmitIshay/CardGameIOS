a4ddfc85bba7de199de9dd8382a35d3470599069
Amitishay <amitishay@gmail.com>
2024-07-08 23:35:38 +0300

    Commit

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var wel_text: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    let nameKey = "savedName"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadSavedName()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadSavedName()
     }
 
-
+    func loadSavedName() {
+        if let savedName = UserDefaults.standard.string(forKey: nameKey) {
+            if !savedName.isEmpty {
+                nameTextField.isHidden = true
+                wel_text.text = "Hello, \(savedName)!"
+            } else {
+                nameTextField.isHidden = false
+                wel_text.text = "Enter your name"
+            }
+        } else {
+            nameTextField.isHidden = false
+            wel_text.text = "Enter your name"
+        }
+    }
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        if let newName = nameTextField.text, !newName.isEmpty {
+            UserDefaults.standard.set(newName, forKey: nameKey)
+            nameTextField.isHidden = true
+            wel_text.text = "Hello, \(newName)!"
+        }
+                
+                nameTextField.resignFirstResponder()    }
 }
 
