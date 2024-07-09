474cd2a60ba9f42b20f09e6f5f501f6e69c5b286
Amitishay <amitishay@gmail.com>
2024-07-09 23:03:19 +0300

    Commit

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Student22 on 08/07/2024.
 //
-
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,10 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var wel_text: UILabel!
     @IBOutlet weak var saveButton: UIButton!
     let nameKey = "savedName"
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
         loadSavedName()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         loadSavedName()
@@ -37,6 +43,7 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func saveButtonTapped(_ sender: UIButton) {
+        locationManager.requestLocation()
         if let newName = nameTextField.text, !newName.isEmpty {
             UserDefaults.standard.set(newName, forKey: nameKey)
             nameTextField.isHidden = true
@@ -45,4 +52,27 @@ class ViewController: UIViewController {
                 
                 nameTextField.resignFirstResponder()    }
 }
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations")
+        
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("got location: \(lat) \(lon)")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+           print("Failed to get location:", error.localizedDescription)
+       }
 
+       func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+           if status == .authorizedWhenInUse {
+               // Permission granted, request location update
+               locationManager.requestLocation()
+           } else {
+               // Handle denial of permission
+               print("Location permission denied")
+           }
+       }}
