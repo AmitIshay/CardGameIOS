4f822bb0c3b20101f9d799953fd5648740a75cd2
Amitishay <amitishay@gmail.com>
2024-07-13 20:20:01 +0300

    Commit

--- CardGame/GameController.swift
+++ CardGame/GameController.swift
@@ -13,13 +13,24 @@ class GameController: UIViewController {
     @IBOutlet weak var right_img: UIImageView!
     @IBOutlet weak var left_img: UIImageView!
     var totalTime = 5 // Total time for the countdown
-
+    
     @IBOutlet weak var right_lbl: UILabel!
-    let cards = [#imageLiteral(resourceName: "1_of_clubs.png") , #imageLiteral(resourceName: "1_of_diamonds.png") , #imageLiteral(resourceName: "1_of_hearts.png") , #imageLiteral(resourceName: "1_of_spades.png") , #imageLiteral(resourceName: "2_of_clubs.png") , #imageLiteral(resourceName: "2_of_diamonds.png") , #imageLiteral(resourceName: "2_of_hearts.png") , #imageLiteral(resourceName: "2_of_spades.png") , #imageLiteral(resourceName: "3_of_clubs.png") , #imageLiteral(resourceName: "3_of_diamonds.png") , #imageLiteral(resourceName: "3_of_hearts.png") , #imageLiteral(resourceName: "3_of_spades.png") , #imageLiteral(resourceName: "4_of_clubs.png") , #imageLiteral(resourceName: "4_of_diamonds.png") , #imageLiteral(resourceName: "4_of_hearts.png") ,
-                 #imageLiteral(resourceName: "4_of_spades.png") , #imageLiteral(resourceName: "5_of_clubs.png") , #imageLiteral(resourceName: "5_of_diamonds.png") , #imageLiteral(resourceName: "5_of_hearts.png") , #imageLiteral(resourceName: "5_of_spades.png") , #imageLiteral(resourceName: "6_of_clubs.png") , #imageLiteral(resourceName: "6_of_diamonds.png") , #imageLiteral(resourceName: "6_of_hearts.png") , #imageLiteral(resourceName: "6_of_spades.png") , #imageLiteral(resourceName: "7_of_clubs.png") , #imageLiteral(resourceName: "7_of_diamonds.png") , #imageLiteral(resourceName: "7_of_hearts.png") , #imageLiteral(resourceName: "7_of_spades.png") , #imageLiteral(resourceName: "8_of_clubs.png") , #imageLiteral(resourceName: "8_of_diamonds.png") ,
-                 #imageLiteral(resourceName: "8_of_hearts.png") , #imageLiteral(resourceName: "8_of_spades.png") , #imageLiteral(resourceName: "9_of_clubs.png") , #imageLiteral(resourceName: "9_of_diamonds.png") , #imageLiteral(resourceName: "9_of_hearts.png") , #imageLiteral(resourceName: "9_of_spades.png") , #imageLiteral(resourceName: "10_of_clubs.png") , #imageLiteral(resourceName: "10_of_diamonds.png") , #imageLiteral(resourceName: "10_of_hearts.png") , #imageLiteral(resourceName: "10_of_spades.png") , #imageLiteral(resourceName: "11_of_clubs.png") , #imageLiteral(resourceName: "11_of_diamonds.png") , #imageLiteral(resourceName: "11_of_hearts.png") , #imageLiteral(resourceName: "11_of_spades.png") , #imageLiteral(resourceName: "12_of_clubs.png") ,
-                 #imageLiteral(resourceName: "12_of_diamonds.png") , #imageLiteral(resourceName: "12_of_hearts.png") , #imageLiteral(resourceName: "12_of_spades.png") , #imageLiteral(resourceName: "13_of_clubs.png") , #imageLiteral(resourceName: "13_of_diamonds.png") , #imageLiteral(resourceName: "13_of_hearts.png") , #imageLiteral(resourceName: "13_of_spades.png")
+    private let cards = [
+        "1_of_clubs", "1_of_diamonds", "1_of_hearts", "1_of_spades",
+        "2_of_clubs", "2_of_diamonds", "2_of_hearts", "2_of_spades",
+        "3_of_clubs", "3_of_diamonds", "3_of_hearts", "3_of_spades",
+        "4_of_clubs", "4_of_diamonds", "4_of_hearts", "4_of_spades",
+        "5_of_clubs", "5_of_diamonds", "5_of_hearts", "5_of_spades",
+        "6_of_clubs", "6_of_diamonds", "6_of_hearts", "6_of_spades",
+        "7_of_clubs", "7_of_diamonds", "7_of_hearts", "7_of_spades",
+        "8_of_clubs", "8_of_diamonds", "8_of_hearts", "8_of_spades",
+        "9_of_clubs", "9_of_diamonds", "9_of_hearts", "9_of_spades",
+        "10_of_clubs", "10_of_diamonds", "10_of_hearts", "10_of_spades",
+        "11_of_clubs", "11_of_diamonds", "11_of_hearts", "11_of_spades",
+        "12_of_clubs", "12_of_diamonds", "12_of_hearts", "12_of_spades",
+        "13_of_clubs", "13_of_diamonds", "13_of_hearts", "13_of_spades",
     ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateImages()
@@ -44,24 +55,26 @@ class GameController: UIViewController {
     func startTimer() {
         countdownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         updateImages()
-
+        
     }
     @objc func updateImages() {
-        let randomIndex = Int.random(in: 0..<cards.count)
-        left_img.image = cards[randomIndex]
-        var rightIndex = Int.random(in: 0..<cards.count)
-        right_img.image = cards[rightIndex]
+        let leftCard = drawCard()
+        let rightCard = drawCard()
+        left_img.image = UIImage(named: leftCard)
+        right_img.image = UIImage(named: rightCard)
+        let leftValue = cardValue(cardName: leftCard)
+        let rightValue = cardValue(cardName: rightCard)
         
-               // Compare indices and update labels
-               if randomIndex > rightIndex {
-                   leftScore += 1
-                   left_lbl.text = "\(leftScore) point"
-                   right_lbl.text = "\(rightScore) point"
-               } else if rightIndex > randomIndex {
-                   rightScore += 1
-                   left_lbl.text = "\(leftScore) point"
-                   right_lbl.text = "\(rightScore) point"
-               }
+        // Compare indices and update labels
+        if leftValue > rightValue {
+            leftScore += 1
+            left_lbl.text = "\(leftScore) point"
+            right_lbl.text = "\(rightScore) point"
+        } else if rightValue > leftValue {
+            rightScore += 1
+            left_lbl.text = "\(leftScore) point"
+            right_lbl.text = "\(rightScore) point"
+        }
     }
     
     @objc func updateTimer() {
@@ -82,5 +95,12 @@ class GameController: UIViewController {
     deinit {
         // Make sure to invalidate the timer when the view controller is deallocated
         countdownTimer?.invalidate()
+    }
+    private func cardValue(cardName: String) -> Int {
+        let components = cardName.split(separator: "_")
+        return Int(components[0]) ?? 0
+    }
+    func drawCard() -> String {
+        return cards[Int(arc4random_uniform(UInt32(cards.count - 1)))]
     }
 }
