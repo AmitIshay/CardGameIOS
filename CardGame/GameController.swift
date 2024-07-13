1d5661c144895cb92a7c738832e6cc88d3cd6e59
Amitishay <amitishay@gmail.com>
2024-07-13 13:01:26 +0300

    Commit

--- CardGame/GameController.swift
+++ CardGame/GameController.swift
@@ -8,10 +8,21 @@ class GameController: UIViewController {
     var locValue: String?
     var nameValue: String?
     var countdownTimer: Timer?
+
+    @IBOutlet weak var right_img: UIImageView!
+    @IBOutlet weak var left_img: UIImageView!
     var totalTime = 5 // Total time for the countdown
+
     @IBOutlet weak var right_lbl: UILabel!
+    let cards = [#imageLiteral(resourceName: "1_of_clubs.png") , #imageLiteral(resourceName: "1_of_diamonds.png") , #imageLiteral(resourceName: "1_of_hearts.png") , #imageLiteral(resourceName: "1_of_spades.png") , #imageLiteral(resourceName: "2_of_clubs.png") , #imageLiteral(resourceName: "2_of_diamonds.png") , #imageLiteral(resourceName: "2_of_hearts.png") , #imageLiteral(resourceName: "2_of_spades.png") , #imageLiteral(resourceName: "3_of_clubs.png") , #imageLiteral(resourceName: "3_of_diamonds.png") , #imageLiteral(resourceName: "3_of_hearts.png") , #imageLiteral(resourceName: "3_of_spades.png") , #imageLiteral(resourceName: "4_of_clubs.png") , #imageLiteral(resourceName: "4_of_diamonds.png") , #imageLiteral(resourceName: "4_of_hearts.png") ,
+                 #imageLiteral(resourceName: "4_of_spades.png") , #imageLiteral(resourceName: "5_of_clubs.png") , #imageLiteral(resourceName: "5_of_diamonds.png") , #imageLiteral(resourceName: "5_of_hearts.png") , #imageLiteral(resourceName: "5_of_spades.png") , #imageLiteral(resourceName: "6_of_clubs.png") , #imageLiteral(resourceName: "6_of_diamonds.png") , #imageLiteral(resourceName: "6_of_hearts.png") , #imageLiteral(resourceName: "6_of_spades.png") , #imageLiteral(resourceName: "7_of_clubs.png") , #imageLiteral(resourceName: "7_of_diamonds.png") , #imageLiteral(resourceName: "7_of_hearts.png") , #imageLiteral(resourceName: "7_of_spades.png") , #imageLiteral(resourceName: "8_of_clubs.png") , #imageLiteral(resourceName: "8_of_diamonds.png") ,
+                 #imageLiteral(resourceName: "8_of_hearts.png") , #imageLiteral(resourceName: "8_of_spades.png") , #imageLiteral(resourceName: "9_of_clubs.png") , #imageLiteral(resourceName: "9_of_diamonds.png") , #imageLiteral(resourceName: "9_of_hearts.png") , #imageLiteral(resourceName: "9_of_spades.png") , #imageLiteral(resourceName: "10_of_clubs.png") , #imageLiteral(resourceName: "10_of_diamonds.png") , #imageLiteral(resourceName: "10_of_hearts.png") , #imageLiteral(resourceName: "10_of_spades.png") , #imageLiteral(resourceName: "11_of_clubs.png") , #imageLiteral(resourceName: "11_of_diamonds.png") , #imageLiteral(resourceName: "11_of_hearts.png") , #imageLiteral(resourceName: "11_of_spades.png") , #imageLiteral(resourceName: "12_of_clubs.png") ,
+                 #imageLiteral(resourceName: "12_of_diamonds.png") , #imageLiteral(resourceName: "12_of_hearts.png") , #imageLiteral(resourceName: "12_of_spades.png") , #imageLiteral(resourceName: "13_of_clubs.png") , #imageLiteral(resourceName: "13_of_diamonds.png") , #imageLiteral(resourceName: "13_of_hearts.png") , #imageLiteral(resourceName: "13_of_spades.png")
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
+        left_img.image = cards[0]
+        right_img.image = cards[0]
         if let locString = locValue, let locDouble = Double(locString) {
             if locDouble < location {
                 lbl_locc.text = locValue
